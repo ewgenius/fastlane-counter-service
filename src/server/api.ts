45c6bffb7a43d1cdab5444e22bc875b3d0de5de3
Evgeniy Khramkov <ewgeniux@gmail.com>
2017-04-09 13:39:52 +0300

    counter api

--- src/server/api.ts
+++ src/server/api.ts
@@ -6,4 +6,20 @@ api.get('/test', (req, res) => {
   res.send(`${req.method} ok`);
 });
 
+api.post('/counter/:id/increment', (req, res) => {
+  console.log(req.params.id);
+  res.send({
+    action: 'increment',
+    id: req.params.id
+  });
+});
+
+api.post('/counter/:id/decrement', (req, res) => {
+  console.log(req.params.id);
+  res.send({
+    action: 'decrement',
+    id: req.params.id
+  });
+});
+
 export default api;
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,25 +1,25 @@
 import * as path from 'path';
 import * as express from 'express';
-import * as mongoose from 'mongoose';
+// import * as mongoose from 'mongoose';
 import { config as configDotenv } from 'dotenv';
 import api from './api';
 
 configDotenv();
 
 const PORT = process.env.PORT || 3000;
-const MONGO_URL = process.env.MONGO_URL;
+// const MONGO_URL = process.env.MONGO_URL;
 const ENV = process.env.NODE_ENV || 'production';
 
 const app = express();
 
 // (mongoose as any).Promise = Promise;
-mongoose.connect(MONGO_URL, {}, (err) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('db connected');
-  }
-});
+// mongoose.connect(MONGO_URL, {}, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log('db connected');
+//   }
+// });
 
 if (ENV === 'development') {
   const webpackMiddleware = require('webpack-dev-middleware');
